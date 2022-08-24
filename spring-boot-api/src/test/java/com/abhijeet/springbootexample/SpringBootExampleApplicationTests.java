36621243b93c21ca64a464f10a3673119deff924
Abhi <abhiran6264@gmail.com>
2022-08-24 18:10:49 +0530

    apring boot api project

---
+++ spring-boot-api/src/test/java/com/abhijeet/springbootexample/SpringBootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhijeet.springbootexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
