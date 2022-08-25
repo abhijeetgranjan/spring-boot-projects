2e65f1dd6bfc0ec07fcfc81d95cad5241c3f7256
Abhi <abhiran6264@gmail.com>
2022-08-25 18:35:26 +0530

    added profiling

--- spring-boot-api/src/main/java/com/abhijeet/springbootexample/controller/ProjectInfoController.java
+++ spring-boot-api/src/main/java/com/abhijeet/springbootexample/controller/ProjectInfoController.java
@@ -1,6 +1,8 @@
 package com.abhijeet.springbootexample.controller;
 
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class ProjectInfoController {
 
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
+
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
+
     @GetMapping("/ping")
     public String getProjectStatus(){
-        return "Project is up and running";
+
+        return
+            "the active profile is" + activeProfile + "\n"+ welcomeMessage;
     }
 
 
